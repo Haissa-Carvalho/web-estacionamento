5a903916ea85c8829205d9f9426174738d8fd958
aluno <aluno>
2019-09-06 15:38:45 -0300

    vamo ver se da certo

--- cad_veiculos.php
+++ cad_veiculos.php
@@ -1,93 +1,106 @@
-<?php
-
+<?php 
 var_dump($_POST);
-
-if (count($_POST) > 0) {
-	// um formulário foi enviado.
-	// inserir no banco de dados
-
-	$banco = "estacionamento";
-	$usuario = "estacionamento";
-	$senha = "joselia";
-
-	// me conecto ao banco de dados usando o PDO
-	$conexao = new PDO("mysql:host=localhost;dbname=${banco}", $usuario, $senha);
-
-	// monto o meu comando SQL em uma string
-	// Os pontos de interrogação definem lugares
-	// no comando que devem ser preenchidos
-	// com dados
-	$sql = "INSERT INTO veiculo VALUES (?, ?, ?, ?)";
-
-	// uso "prepare" para criar o comando a partir da minha string
-	$comando = $conexao->prepare($sql);
-
-	// uso "execute" para executar o comando SQL.
-	// Passo os dados esperados pelo comando em um vetor (array)
-	// Os dados passados seguem a ordem dos pontos de interrogação acima
-	// Se o comando funcionar, "execute" retornará TRUE
-	// Caso contrário, "execute" retornará FALSE
-	$sucesso = $comando->execute([
-		$_POST['placa'],
-		$_POST['modelo_codmod'],
-		$_POST['cliente_cpf'],
-		$_POST['cor']
-	]);
-
-	// se o comando for bem sucedido, monto uma mensagem amigável
-	$mensagem = '';
-	if ($sucesso)
-	{
-		$mensagem = "Veículo cadastrado!";
-	}
-	else
-	{
-		// se deu erro, a mensagem não será tão amigável :(
-		$mensagem = "Erro: " . $comando->errorInfo()[2];
-	}
-
-	// uso um cookie para passar a mensagem para a página de clientes
-	setcookie('mensagem', $mensagem);
-
-	// redireciona para a página clientes.php
-	header('Location: veiculos.php');
-}
-
-?>
-
- <!DOCTYPE html>
+if(count($_POST)>0){
+    echo '<br>' . $_POST['placa'];
+    echo '<br>' . $_POST['modelo'];
+    echo '<br>' . $_POST['CPF'];
+    echo '<br>' . $_POST['cor'];
+   
+   
+    
+    $conexao = new PDO("mysql:host=localhost;dbname=estacionamento", "estacionamento", "joselia");
+    $sql = "insert into veiculo values (?,?,?,?)";
+    $comando = $conexao->prepare($sql);
+    $sucesso = $comando->execute([
+        $_POST['placa'],
+        $_POST['modelo_codmod'],
+        $_POST['cliente_cpf'],
+        $_POST['cor'],
+       
+        ]);
+    $mensagem = '';
+    if ($sucesso)
+    {
+        $mensagem = "Modelo cadastrado!";
+    }
+    else
+    {
+        // se deu erro, a mensagem não será tão amigável :(
+        $mensagem = "Erro: " . $comando->errorInfo()[2];
+    }
+    setcookie('mensagem', $mensagem);
+    header('Location:veiculos.php');
+} // fechou o if
+$conexao = new PDO("mysql:host=localhost;dbname=estacionamento", "estacionamento", "joselia");
+$sql = "SELECT * FROM Modelo";
+$comando = $conexao->query($sql);
+$modelos = $comando->fetchAll();
+ ?>
+<DOCTYPE html>
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
  	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
- 	<title>Novo Veiculo - IF Park</title>
+ 	<title>Clientes - IF Park</title>
  	<link rel="stylesheet" href="css/estilo.css">
  </head>
  <body>
  	
 	<header>
 		<h1>ℙ IF Park</h1>
+        <nav>
+            <ul id="menu">
+                <li><a href="estacionados.php">Estacionados</a></li>
+                <li><a href="patio.php">Pátios</a></li>
+                <li class="ativo"><a href="clientes.php">Clientes</a></li>
+                <li><a href="veiculos.php">Veículos</a></li>
+                <li><a href="modelos.php">Modelos</a></li>
+            </ul>
+        </nav>
 	</header>
 	<div id="container">
+            <?php if(!empty($mensagem)): ?>
+                <div id="mensagem">
+                    <?= $mensagem; ?>
+                </div>
+            <?php endif; ?>
 		<main>
-			<h2>Novo Veiculo</h2>
+			<h2>Cadastro dos modelos</h2>
+    		<form action="cad_veiculos.php" method="post">
+        <p>
+            <label for="iplaca">Placa:</label><br>
+            <input type="text" id="iplaca" name="placa">
+        </p>
+        <p>
+            <label for="imodelo">Modelo:</label><br>
+            <!--input type="text" id="imodelo" name="modelo"-->
+            <select id="imodelo" name="modelo_codmod">
+                
+                <?php foreach ($modelos as $m): ?>
 
-			<form action="cad_veiculos.php" method="post">
-				<p><label for="iplaca">Placa:</label></p>
-				<p><input type="text" id="iplaca" name="placa"></p>
+                    <option value="<?= $m['codmod'] ?>">
+                        <?= $m['desc_2'] ?>
+                    </option>
 
-				<p><label for="imodelo_codmod">Código do modelo:</label></p>
-				<p><input type="text" id="imodelo_codmod" name="modelo_codmod"></p>
+                <?php endforeach; ?>
 
-				<p><label for="icliente_cpf">Cliente CPF</label></p>
-				<p><input type="number" id="icliente_cpf" name="cliente_cpf"></p>
+            </select>     
+        </p>
+        <p>
+            <label for="icpf">CPF:</label><br>
+            <input type="number" id="icpf" name="cliente_cpf">
+        </p>
+        <p>
+            <label for="iCor">Cor:</label><br>
+            <input type="text" id="icor" name="cor">
+        </p>
+    
+        	<button type="submit">Enviar</button>
+        </p>
 
-				<p><label for="icor">Cor:</label></p>
-				<p><input type="text" id="icor" name="cor"></p>
 
-				<p><button type="submit">Salvar</button></p>
-			</form>
 
+			
 		</main>
 	</div>
 	<footer>
--- veiculos.php
+++ veiculos.php
@@ -2,10 +2,22 @@
 
 	$conexao = new PDO("mysql:host=localhost;dbname=estacionamento", "estacionamento", "joselia");
 
-	$sql = "SELECT * FROM veiculo";
+	/*$sql = "SELECT * FROM veiculo";*/
+	/*$sql = "SELECT placa, desc_2, cliente_cpf, cor FROM veiculo, Modelo WHERE modelo_codmod = codmod";*/
+
+	$sql = <<<EOL
+	SELECT placa, desc_2, Nome, cor
+	  FROM veiculo, Modelo, Cliente
+	WHERE modelo_codmod = codmod
+	  AND cliente_cpf = cpf
+EOL;
+
+
 	$resultado = $conexao->query($sql);
 
-	$veiculo = $resultado->fetchAll();
+	$veiculos = $resultado->fetchAll();
+
+	// verifico se tem mensagem pra ser exibida ao usuário.
 	$mensagem = "";
 	if (isset($_COOKIE['mensagem']))
 	{
@@ -13,27 +25,16 @@
 
 		// depois que exibo a mensagem, devo retirá-la
 		// dos cookies.
-		setcookie('mensagem', '', 1);}
+		setcookie('mensagem', '', 1);
+	}
 
-	/*[
-		[
-			'cpf'=>'04080660608',
-			'nome'=>'Livão',
-			'dtNasc'=>'28/11/00'
-		],
-		[
-			'cpf'=>'15107352604',
-			'nome'=>'Livinha',
-			'dtNasc'=>'14/01/02'
-		]
-	];*/
  ?>
  <!DOCTYPE html>
  <html lang="en">
  <head>
  	<meta charset="UTF-8">
  	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
- 	<title>Veiculos - IF Park</title>
+ 	<title>Veículos - IF Park</title>
  	<link rel="stylesheet" href="css/estilo.css">
  </head>
  <body>
@@ -43,15 +44,16 @@
 		<nav>
 			<ul id="menu">
 				<li><a href="estacionados.php">Estacionados</a></li>
-				<li><a href="patio.php">Pátios</a></li>
-				<li class="ativo"><a href="clientes.php">Clientes</a></li>
-				<li><a href="veiculos.php">Veículos</a></li>
+				<li><a href="patios.php">Pátios</a></li>
+				<li><a href="clientes.php">Clientes</a></li>
+				<li class="ativo"><a href="veiculos.php">Veículos</a></li>
 				<li><a href="modelos.php">Modelos</a></li>
 			</ul>
 		</nav>
 	</header>
 	<div id="container">
 		<main>
+			<h2>Veículos</h2>
 
 			<?php if(!empty($mensagem)): ?>
 				<div id="mensagem">
@@ -59,28 +61,35 @@
 				</div>
 			<?php endif; ?>
 
-			<h2>Veículos</h2>
-			<a href="cad_veiculos.php">Cadastrar Novo Veículo</a>
+			<p><a href="cad_veiculos.php">Novo Veículo</a></p>
+
+			<?php if (count($veiculos) == 0): ?>
+
+				<p>Não há nenhum registro.</p>
+
+			<?php else: ?>
+
 			<table class="tabela-dados">
 					<thead>
 						<tr>
 							<th>Placa</th>
-							<th>Código do modelo</th>
-							<th>CPF</th>
+							<th>Modelo</th>
 							<th>Cor</th>
+							<th>Cliente</th>
 						</tr>
 					</thead>
 					<tbody>
-						<?php foreach ($veiculo as $veiculo): ?>
+						<?php foreach ($veiculos as $veiculo): ?>
 						<tr>
 							<td><?= $veiculo['placa'] ?></td>
-							<td><?= $veiculo['modelo_codmod'] ?></td>
-							<td><?= $veiculo['cliente_cpf'] ?></td>
+							<td><?= $veiculo['desc_2'] ?></td>
 							<td><?= $veiculo['cor'] ?></td>
+							<td><?= $veiculo['Nome'] ?></td>
 						</tr>
 						<?php endforeach; ?>
 					</tbody>
 				</table>	
+			<?php endif; ?>
 		</main>
 	</div>
 	<footer>
